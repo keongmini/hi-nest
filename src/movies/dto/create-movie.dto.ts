96df1c8a1e4e2f5f339e3c1e0950cac6a0e6ed4a
keongmini <kmnam09@gmail.com>
2023-01-15 20:31:51 +0900

    add update dto

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,12 +1,15 @@
 // DTO(Data Transfer Object) 데이터 전송 객체
 
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString, IsOptional } from 'class-validator';
 
 export class CreateMovieDto {
   @IsString()
   readonly title: string;
+
   @IsNumber()
   readonly year: number;
+
+  @IsOptional()
   @IsString({ each: true })
   readonly genres: string[];
 }
